68c56000c6d9609586dc44e6ae2e5f6e1d1e1d74
Zhi.Huang <zh.impact@outlook.com>
2026-05-01 02:40:32 +0800

    update: better DX for cli

--- packages/issue-context-collector/src/index.ts
+++ packages/issue-context-collector/src/index.ts
@@ -3,20 +3,31 @@ import { Octokit } from '@octokit/core'
 
 const octokit = new Octokit({ auth: process.env.GITHUB_PAT })
 
-const response = await octokit.request('GET /repos/{owner}/{repo}/issues', {
-  headers: {
-    'X-GitHub-Api-Version': '2026-03-10',
-  },
-  owner: 'facebook',
-  repo: 'react',
-  state: 'all',
-  sort: 'comments',
-})
+async function main() {
+  const response = await octokit.request('GET /repos/{owner}/{repo}/issues', {
+    headers: {
+      'X-GitHub-Api-Version': '2026-03-10',
+    },
+    owner: 'facebook',
+    repo: 'react',
+    state: 'all',
+    sort: 'comments',
+  })
+  return response
+}
 
-console.log(
-  response.data.map((issue) => ({
-    title: issue.title,
-    comments: issue.comments,
-    url: issue.html_url,
-  })),
-)
+if (import.meta.main) {
+  async function cli() {
+    const output = process.argv[2] ?? 'output.json'
+    if (!output) {
+      console.error('Usage:  <output-file>')
+      process.exit(1)
+    }
+
+    const { writeFile } = await import('node:fs/promises')
+
+    const data = await main()
+    await writeFile(output, JSON.stringify(data, null, 2))
+  }
+  cli()
+}
